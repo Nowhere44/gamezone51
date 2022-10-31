48fdf2fadce5a499b7f9a331b3bb8a48d31426ec
Nowhere44 <omaralmoctarcoulibaly@gmail.com>
2022-10-31 21:31:01 +0100

    fix registerForm

--- src/Repository/GameLauncherRepository.php
+++ src/Repository/GameLauncherRepository.php
@@ -94,7 +94,7 @@ class GameLauncherRepository extends ServiceEntityRepository
             'SELECT p
             FROM App\Entity\GameLauncher as p
             WHERE
-                    (p.title like :title)'
+                    (UPPER(p.title) like UPPER(:title))'
         )->setParameter('title', $title);
 
         return $query->execute();
